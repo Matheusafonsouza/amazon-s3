e0e2f7ed7f5f07495e4f2339351bbfbbbe9a602e
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-29 00:25:22 -0300

    feat: uploading file

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ if __name__ == '__main__':
     # This is for create a bucket
     # s3_client.create_bucket('afonsotest123')
 
+    # This is for upload file inside  a bucket
+    # with open('./files/example.txt', 'rb') as file:
+    #     s3_client.upload_file(file, 'afonsotest123')
+
     # This is for list buckets
     print(s3_client.buckets)
--- s3/services.py
+++ s3/services.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import boto3 as boto
 from botocore.exceptions import ClientError
@@ -21,6 +22,17 @@ class S3:
                     Bucket=name,
                     CreateBucketConfiguration=location
                 )
+        except ClientError as e:
+            logging.error(e)
+            return False
+        return True
+
+    def upload_file(self, file, bucket, object_name=None):
+        if object_name is None:
+            object_name = os.path.basename(file.name)
+
+        try:
+            self.sdk.upload_fileobj(file, bucket, object_name)
         except ClientError as e:
             logging.error(e)
             return False
