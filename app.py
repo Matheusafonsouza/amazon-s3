3d8ac984eedd59770eea8084a4a53ea334f4d53a
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-29 00:51:24 -0300

    fix: enable all in app

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ if __name__ == '__main__':
     s3_client = S3()
 
     # This is for create a bucket
-    # s3_client.create_bucket('afonsotest123')
+    s3_client.create_bucket('afonsotest123')
 
     # This is for upload file inside  a bucket
-    # s3_client.upload('./files/example.txt', 'afonsotest123', 'cool.txt')
+    s3_client.upload('./files/example.txt', 'afonsotest123', 'cool.txt')
 
     # This is for download a file
-    # s3_client.download('./downloads/cool.txt',
-    #                    'afonsotest123', 'testelegal.txt')
+    s3_client.download('./downloads/cool.txt',
+                       'afonsotest123', 'testelegal.txt')
 
     # This is for list objects of bucket
     objects = s3_client.list_bucket_objects('afonsotest123')
